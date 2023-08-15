7b62e62cfeb97db9d636e539d56dfdc860d87586
abdouhamim <abderahmane.hamim@gmail.com>
2023-08-16 01:54:38 +0200

    finishing home page frontend css html

--- src/app/carossel/carossel.component.ts
+++ src/app/carossel/carossel.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-carossel',
   templateUrl: './carossel.component.html',
   styleUrls: ['./carossel.component.css']
 })
-export class CarosselComponent {
+export class CarosselComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    // Initialize Swiper here
+    new Swiper('.clients-slider', {
+      speed: 400,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 40
+        },
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 60
+        },
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 80
+        },
+        992: {
+          slidesPerView: 6,
+          spaceBetween: 120
+        }
+      }
+    });
+  }
 }
--- src/app/profilemed/profilemed.component.ts
+++ src/app/profilemed/profilemed.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-profilemed',
   templateUrl: './profilemed.component.html',
   styleUrls: ['./profilemed.component.css']
 })
 export class ProfilemedComponent {
-
+  
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 }
