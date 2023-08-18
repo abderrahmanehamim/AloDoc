9d885547fdc1ddbf9083201b983d6f3657b9270c
abdouhamim <abderahmane.hamim@gmail.com>
2023-08-18 21:11:49 +0200

    adding filters to chercher médcin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,9 +20,15 @@ import { ProfilemedComponent } from './profilemed/profilemed.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatChipsModule} from '@angular/material/chips';
 @NgModule({
   declarations: [
-    
+  
     AppComponent,
     HeadernavbarComponent,
     HomeComponent,
@@ -42,11 +48,18 @@ import { HousingLocationComponent } from './housing-location/housing-location.co
   
   ],
   imports: [
+    MatChipsModule,
+    NgFor,
     RouterModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgSelectModule
+    NgSelectModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/rechercher-med/rechercher-med.component.ts
+++ src/app/rechercher-med/rechercher-med.component.ts
@@ -1,13 +1,79 @@
 import { Component, inject } from '@angular/core';
 import { Housinglocation } from '../housinglocation';
+import {MatChipsModule} from '@angular/material/chips';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { ProfiledoctorService } from '../profiledoctor.service';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 @Component({
   selector: 'app-rechercher-med',
   templateUrl: './rechercher-med.component.html',
   styleUrls: ['./rechercher-med.component.css']
 })
 export class RechercherMedComponent {
+  myControl = new FormControl('');
+  options: string[] = [
+                                                                      'Addictologue',
+                                                                      'Algologue - Traitement de la douleur',
+                                                                      'Allergologue',
+                                                                      'Allergologue-pédiatrique',
+                                                                      'Anatomopathologiste',
+                                                                      'Andrologue',
+                                                                      'Anesthésiste-réanimateur',
+                                                                      'Angiologue',
+                                                                      'Audioprothésiste',
+                                                                      'Auriculothérapie',
+                                                                      'Biologiste (Laboratoire d\'analyse)',
+                                                                      'Cardiologue',
+                                                                      'Cardiologue interventionnel',
+                                                                      'Cardiologue pédiatrique',
+                                                                      'Cardiologue rythmologue',
+                                                                      'Centre de Dialyse',
+                                                                      'Chiropracteur',
+                                                                      'Chirurgien cervico-facial',
+                                                                      'Chirurgie buccale',
+                                                                      'Chirurgien cardiaque',
+                                                                      'Chirurgien cardiaque pédiatrique',
+                                                                      'Chirurgien de l\'obésité',
+                                                                      'Chirurgien de la main',
+                                                                      'Chirurgien digestif',
+                                                                      'Chirurgien du foie, du pancréas et des voies biliaires',
+                                                                      'Chirurgien esthétique et plastique',
+                                                                      'Chirurgien général',
+                                                                      'Chirurgien maxillo-facial',
+                                                                      'Chirurgien orthopédiste et traumatologue',
+                                                                      'Chirurgien orthopédiste pédiatrique',
+                                                                      'Chirurgien pédiatre',
+                                                                      'Chirurgien thoracique',
+                                                                      'Chirurgien vasculaire',
+                                                                      'Chirurgien viscéral',
+                                                                      'Chirurgien viscéral pédiatrique',
+                                                                      'Coach développement personnel',
+                                                                      'Coach sportif',
+                                                                      'Dentiste',
+                                                                      'Dermatologue',
+                                                                      'Dermatologue pédiatrique',
+                                                                      'Diabétologue',
+                                                                      'Diététicien',
+                                                                      'Endocrinologue',
+                                                                      'Endodontiste',
+                                                                      'Ergothérapie',
+                                                                      'Esthétique dentaire',
+                                                                      'Ethiopathe',
+                                                                      'Gastro-entérologue',
+                                                                      'Gastro-entérologue pédiatrique',
+                                                                      'Gynécologue',
+                                                                      'Gynécologue-obstétricien',
+                                                                      'Généticien',
+                                                                      'Gérontologue - Gériatre',
+                                                                      'Homéopathe',
+                                                                      'Hypnothérapeute',
+                                                                      'Hématologue',
+                                                                      'Hépatologue'
+  ];
+
   DoctorprofileList: Housinglocation[] = [];
   profiledoctorService: ProfiledoctorService = inject(ProfiledoctorService);
   constructor(){
